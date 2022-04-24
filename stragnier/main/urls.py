df1bfaa6c86c7e2820ac4871da830b1c2a1a38dd
Sakengali <sakengali.kazhiyev@nu.edu.kz>
2022-04-24 15:41:11 +0600

    added flexible url

--- stragnier/main/urls.py
+++ stragnier/main/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views #import from this dir
 
 urlpatterns=[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<str: url_val>', views.get_url, url_val = 'url')
 ]
 
 
--- stragnier/main/views.py
+++ stragnier/main/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Yep")
+
+def get_url(request, url_val):
+    return HttpResponse(f'url is {url_val}')

5834907535ede8d5636068c1258203e48e0f6d9b
Sakengali <sakengali.kazhiyev@nu.edu.kz>
2022-04-25 18:26:58 +0600

    added request.html, request submit and show

--- stragnier/main/urls.py
+++ stragnier/main/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from . import views #import from this dir
 
+app_name = "main"
+
 urlpatterns=[
     path('', views.index, name='index'),
-    path('<str:url_val>', views.get_url) #take string, and pass tp get_url
+    path('requests', views.requests, name='requests') 
 ]
 
 
 
-#token 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
+#token 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
--- stragnier/main/views.py
+++ stragnier/main/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django import forms
 
-# Create your views here.
+#form for request submission
+class NewBookForm(forms.Form):
+    book_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'book name'}))
+    book_url = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'book url aws/abebooks'}))
+
+
+#list-database
+requests_info = []
 
 def index(request):
-    return render(request, "main/main.html")
-
-def get_url(request, url_val):
-    return render(request, "main/test.html", {
-        "url_1": url_val[0],
+    return render(request, "main/main.html", {
+        "form": NewBookForm
     })
 
+def requests(request):
+    if request.method == 'POST':
+        #a form object; request.POST - gives all values of form
+        form = NewBookForm(request.POST)        
+
+        if form.is_valid():
+            #cleaned.data is a way to access data
+            book_name = form.cleaned_data['book_name']   
+            book_url = form.cleaned_data['book_url']   
+
+            #append a dict of data to the list 
+            requests_info.append({"book_name": book_name, "book_url" : book_url})            
+        else:
+            #if form is invalid, load main again
+            render(request, "main/main.html", {
+                "form": NewBookForm
+            })
+    print(f"requests: {requests_info}")
+    return render(request, "main/requests.html",{
+        "requests_info": requests_info
+    })
+
+
+#TODO: urlfield; ...
